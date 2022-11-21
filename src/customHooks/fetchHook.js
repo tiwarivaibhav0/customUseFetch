dd759626fcaebd31153f85690f54ddd3b104b40e
vaibhavkumartiwari-cedcommerce <vaibhavkumartiwari@cedcoss.com>
2022-11-21 11:13:10 +0530

    updated

--- src/customHooks/fetchHook.js
+++ src/customHooks/fetchHook.js
@@ -3,15 +3,20 @@ import React, { useEffect, useState } from "react";
 const useFetch = () => {
   const [data, setState] = useState([]);
 
-  const extractDataFromApi = (urlPassed = "", payload = "", method = "GET") => {
+  const extractDataFromApi = (urlPassed = "", payload = "", method = "GET",headers={}) => {
     let url = new URL(urlPassed);
+    let data ={
+        headers: headers,
+        method: method,
+      }
+    if(method==="GET")
     for (let i in payload) {
       url.searchParams.append(i, payload[i]);
     }
-    fetch(url, {
-      headers: {},
-      method: method,
-    })
+    else
+    data["body"]=JSON.stringify(payload)
+
+    fetch(url, data)
       .then((res) => res.json())
       .then((dataFetched) => setState(dataFetched));
   };
